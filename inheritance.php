15375037188483030785024cc9342284e263eaf9
Dede Aminudin <rasbulloe@gmail.com>
2019-10-02 21:42:18 +0700

    overriding

--- inheritance.php
+++ inheritance.php
@@ -26,7 +26,7 @@ class Produk {
 
 
 	public function getInfoProduk() {
-		$str = "{$this->tipe}: {$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
+		$str = " {$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
 		
 
 		return $str;
@@ -37,7 +37,7 @@ class Produk {
 
 class Komik extends Produk {
 	public function getInfoProduk() {
-		$str = "Komik : {$this->judul} | {$this->getLabel()} (Rp. {$this->harga}) - {$this->jmlHalaman} Halaman.";
+		$str = "Komik : " .parent::getInfoProduk(). " - {$this->jmlHalaman} Halaman.";
 		return $str;
 	}
 }
@@ -60,7 +60,8 @@ class CetakInfoProduk {
 
 
 	$produk1 = new Komik("Naruto","Masashi Kishimoto","Shonen Jump",30000,100, 0,"Komik");
-	$produk2 = new Produk("Uncharted","Neil Druckmann","Sony Computer",250000,0,50,"Game");
+	$produk2 = new Game("Uncharted","Neil Druckmann","Sony Computer",250000,0,50,"Game");
+
 
 		
 	echo $produk1->getInfoProduk();
